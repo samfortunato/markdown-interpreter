59aa66ee4d3376cfa4316ec45524d65e20b37691
Samuel Fortunato <samfortunato@gmail.com>
2019-04-05 12:16:15 -0400

    Add basic H1 element conversion

--- js/index.js
+++ js/index.js
@@ -2,5 +2,31 @@ const markdownTextarea = document.querySelector('#markdown');
 const interpretedMarkdownArea = document.querySelector('#interpreted-markdown');
 
 markdownTextarea.addEventListener('input', (evt) => {
-  interpretedMarkdownArea.innerHTML = evt.target.value;
+  const markdown = convertMarkdownToH1(evt.target.value);
+
+  if (markdown !== null) {
+    interpretedMarkdownArea.appendChild(markdown);
+  }
 });
+
+const markdownSyntaxMatchers = {
+  h1: /^#\s{1,}(.+)\n?$/,
+  h2: /^##\s{1,}(.+)\n?$/
+};
+
+const convertMarkdownToHTML = (markdown) => {
+
+};
+
+const convertMarkdownToH1 = (markdown) => {
+  const regExMatch = markdown.match(markdownSyntaxMatchers.h1);
+  
+  if (regExMatch) {
+    const h1 = document.createElement('h1');
+    h1.textContent = regExMatch[1];
+
+    return h1;
+  } else {
+    return null;
+  }
+};
